44117655a61e4c1b9c9ef5169c4ae1258a5d8923
reesericci <me@reeseric.ci>
2024-05-31 16:32:50 -0500

    downcase username when creating postmark

--- app/controllers/identity/postmarks_controller.rb
+++ app/controllers/identity/postmarks_controller.rb
@@ -3,7 +3,7 @@ class Identity::PostmarksController < ApplicationController
   end
 
   def create
-    @member = Member.find_by(username: member_params[:username])
+    @member = Member.find_by(username: member_params[:username].downcase)
     Identity::PostmarkMailer.with(postmarkable: @member).new.deliver_later
     redirect_to new_identity_login_path, status: :see_other, flash: {postmark_member_username: @member.username}
   end
