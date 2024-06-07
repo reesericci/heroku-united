cd4838cf612a5cae2739c5ec2aa06fbd4d933f4e
reesericci <me@reeseric.ci>
2024-06-07 03:10:45 +0000

    Simplify expirable
    Patch by Matt Almeida (@northeastprince) <matt7@hey.com>

--- app/models/concerns/expirable.rb
+++ app/models/concerns/expirable.rb
@@ -1,33 +1,11 @@
 module Expirable
   extend ActiveSupport::Concern
 
-  def expired?
-    if !expires_at.nil?
-      expires_at < DateTime.now
-    else
-      false
-    end
+  included do
+    scope :expired, -> { where "expires_at < NOW()" }
   end
 
-  class_methods do
-    def unexpired
-      select do |m|
-        if !m.expires_at.nil?
-          m.expires_at.future?
-        else
-          true
-        end
-      end
-    end
-
-    def expired
-      select do |m|
-        if !m.expires_at.nil?
-          m.expires_at.past?
-        else
-          false
-        end
-      end
-    end
+  def expired?
+    expires_at&.< DateTime.now
   end
 end
