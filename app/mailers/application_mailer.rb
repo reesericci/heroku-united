574af3ff8c082464d8c7e0804b0c7a94e991e26f
reesericci <me@reeseric.ci>
2024-06-16 22:19:50 -0500

    more safe nav

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: email_address_with_name("#{Config.smtp&.dig(:box) || "changeme"}@#{Config.smtp&.dig(:domain) || "example.com"}", Config.organization || "United"),
-    user_name: Config.smtp[:username],
-    password: Config.smtp[:password],
-    address: Config.smtp[:server],
-    port: Config.smtp[:port].to_i,
-    domain: Config.smtp[:domain],
+    user_name: Config.smtp&.dig(:username),
+    password: Config.smtp&.dig(:password),
+    address: Config.smtp&.dig(:server),
+    port: Config.smtp&.dig(:port)&.to_i,
+    domain: Config.smtp&.dig(:domain),
     authentication: :cram_md5,
     enable_starttls: true,
     reply_to: Config.email
