73d30fdeeb4a6ef9400d2a1185c2c56de9d4f163
reesericci <me@reeseric.ci>
2024-06-25 11:37:12 -0500

    set default options

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,17 @@ class ApplicationMailer < ActionMailer::Base
   after_action :set_delivery_options
 
   layout "mailer"
-  
+
+  default from: email_address_with_name("#{Config.smtp&.dig(:box) || "changeme"}@#{Config.smtp&.dig(:domain) || "example.com"}", Config.organization || "United"),
+    user_name: Config.smtp&.dig(:username),
+    password: Config.smtp&.dig(:password),
+    address: Config.smtp&.dig(:server),
+    port: Config.smtp&.dig(:port)&.to_i,
+    domain: Config.smtp&.dig(:domain),
+    authentication: :cram_md5,
+    enable_starttls: true,
+    reply_to: Config.email
+
   private
 
   def set_delivery_options
@@ -18,5 +28,4 @@ class ApplicationMailer < ActionMailer::Base
       reply_to: Config.email
     })
   end
-  
 end
