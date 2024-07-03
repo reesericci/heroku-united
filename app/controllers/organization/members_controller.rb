9fe6539d3a70fd0498e4ca0f0d0e2c1f42612b90
reesericci <me@reeseric.ci>
2024-07-03 07:32:22 -0500

    refactor organization members

--- app/controllers/organization/members_controller.rb
+++ app/controllers/organization/members_controller.rb
@@ -19,18 +19,6 @@ class Organization::MembersController < Organization::BaseController
       member.resurrect
     end
     member.update!(member_params)
-    if member.address
-      member.address.update!(address_params)
-    elsif Address.new(address_params.merge({addressable_id: member.id, addressable_type: "Member"})).valid?
-      member.update!(address: Address.new(address_params.merge({addressable_id: member.id, addressable_type: "Member"})))
-    end
-    params[:member][:extensions_attributes].each do |i|
-      if member.extensions.find_by(name: i[1].keys.first).present?
-        member.extensions.find_by(name: i[1].keys.first).update!(content: i[1][i[1].keys.first])
-      else
-        member.extensions.create!(name: i[1].keys.first, content: i[1][i[1].keys.first])
-      end
-    end
     redirect_to organization_members_path
   end
 
@@ -47,10 +35,6 @@ class Organization::MembersController < Organization::BaseController
   private
 
   def member_params
-    params.require(:member).permit(:name, :username, :email, :expires_at, :banned)
-  end
-
-  def address_params
-    params.require(:member).permit(address_attributes: [:line1, :line2, :city, :province, :code, :country])[:address_attributes]
+    params.require(:member).permit(:name, :username, :email, :expires_at, :banned, address_attributes: [:line1, :line2, :city, :province, :code, :country], extensions_attributes: {})
   end
 end
