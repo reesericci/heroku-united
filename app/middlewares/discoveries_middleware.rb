cbef53b004a7987275ad7d9eb1e1a8800594fa6d
reesericci <me@reeseric.ci>
2024-07-15 12:25:01 -0500

    update middleware

--- app/middlewares/discoveries_middleware.rb
+++ app/middlewares/discoveries_middleware.rb
@@ -5,7 +5,7 @@ class DiscoveriesMiddleware
   
   def call(env)
     # reroute webfinger if basic credentials are provided to the API
-    if env["PATH_INFO"] == "/.well-known/webfinger" && ActionController::HttpAuthentication::Basic.has_basic_credentials?(ActionDispatch::Request.new(env))
+    if env["PATH_INFO"] == "/.well-known/webfinger"       
       Api::Member::Actor::DiscoveriesController.action(:new).call(env)
     else
       @app.call(env)
